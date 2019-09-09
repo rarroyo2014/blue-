4f7f129361db1d525c6f8c90f40084b5a047563d
Rebeca Arroyo <rarroyo2014@fau.edu>
2019-09-09 05:38:26 +0000

    Home button

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,4 +4,8 @@ class QuotesController < ApplicationController
     #I am going to store my first quote from my database into a variable called @quote. I will also edit to enable random quotes to appear rather than first
     @quote = Quote.all.sample
   end 
+
+ def new
+    @quote = Quote.new
+  end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
     root 'quotes#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :quotes
+
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
