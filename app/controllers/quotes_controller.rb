a32ee77e77c2f908fd0da7d387d7d3f95434908d
Rebeca Arroyo <rarroyo2014@fau.edu>
2019-09-09 16:52:09 +0000

    Finishing touches

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -10,11 +10,16 @@ class QuotesController < ApplicationController
   end
 
   def create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
     redirect_to root_path
-
   end
 
+  def about
+  end 
+  
   private
 
   def quote_params
